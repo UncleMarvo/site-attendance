fe730912857720e3d06419b287ac15b2f1dcfaf3
UncleMarvo <eddie_heff@yahoo.com>
2025-10-17 09:57:36 +0100

    Add Program.cs with Main entry point

---
+++ mobile/SiteAttendance.App/Program.cs
@@ -0,0 +1,13 @@
+using Microsoft.Maui;
+using Microsoft.Maui.Hosting;
+
+namespace SiteAttendance.App;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        var app = MauiProgram.CreateMauiApp();
+        app.Run();
+    }
+}
